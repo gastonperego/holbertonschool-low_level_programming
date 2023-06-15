812eb54fe2b937d2a3106d103acd2ae3ccdfa33c
gastonperego <gastonperego@gmail.com>
2023-06-15 10:18:11 -0700

    Display a random number and its last digit, then say if the last digit is higher than 5, less than 6 or zero

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Display a random number and its last digit, then say if the last digit is higher than 5, less than 6 or zero
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d and is",n, n % 10);
+	if (n % 10 > 5)
+	{
+		printf(" greater than 5");
+	} else if (n % 10 < 6 && (n % 10) != 0)
+	{
+		printf(" less than 6 and not 0");
+	} else 
+		printf(" 0");
+	return (0);
+}
