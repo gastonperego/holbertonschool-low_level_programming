6df88f60e1cda4fe5c6acc5c449b9bc46070b0c6
gastonperego <gastonperego@gmail.com>
2023-06-15 10:49:29 -0700

    Print the alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,10 +2,16 @@
 /**
  * main- Display the alphabet in lowercase, followed by a new line
  *
- * Return: lawas zero
+ * Return: always zero
  */
 int main(void)
 {
-	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	char a = a
+	char b = b
+
+	for (a < z)
+	{
+		putchar(a++);
+	}
 	return (0);
 }
