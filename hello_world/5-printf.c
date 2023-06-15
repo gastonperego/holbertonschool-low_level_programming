295e5ad1f7d4799478dbccec649be9f63a085821
gastonperego <gastonperego@gmail.com>
2023-06-15 05:58:30 -0700

    Print text printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio>
 /**
  * main- Prints a text
- * 
+ *
  * Return: always retusrn nothing
  */
-int main (void)
+int main(void)
 {
-	printf("with propper grammar, but the outcome is a piece of art")
-	Return: (0)
+	printf("with propper grammar, but the outcome is a piece of art");
+	Return(0);
+}
