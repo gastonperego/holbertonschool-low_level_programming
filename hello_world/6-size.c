7f71a7ff1c09110acabac49bdcde53f3f81f0ddc
gastonperego <gastonperego@gmail.com>
2023-06-15 06:42:00 -0700

    Print the size of types of variables

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char = %2d byte(s) \n", sizeof(long int));
-	printf("Size of an int= %2d byte(s) \n", sizeof(float));
-	printf("Size of a long int in C = %2d byte(s) \n", sizeof(double));
-	printf("Size of a long long int = %2d byte(s) \n", sizeof(long double));
-	printf("Size of a float = %2d byte(s) \n", sizeof(char));
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %2d byte(s)\n", sizeof(float));
 	return (0);
 }
