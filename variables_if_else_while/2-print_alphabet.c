8be7c7c7f892748a295942ef798338e7d47377aa
gastonperego <gastonperego@gmail.com>
2023-06-15 10:35:44 -0700

    print de alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Display the alphabet in lowercase, followed by a new line
+ *
+ * Return: lawas zero
+ */
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz");
+	return (0);
+}
