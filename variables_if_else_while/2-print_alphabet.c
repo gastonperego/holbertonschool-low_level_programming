99c80dcfefb32457059cdf4b3759db7a19ab5f46
gastonperego <gastonperego@gmail.com>
2023-06-16 14:46:31 -0700

    Print the alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -13,5 +13,6 @@ int main(void)
 	{
 		putchar(a++);
 	}
+	putchar("\n")
 	return (0);
 }
---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main- Should print the alphabet twice, firt lowercase and then uppercase
+ *
+ *  Return: always zero
+ */
+
+int main(void)
+{
+	char
+
