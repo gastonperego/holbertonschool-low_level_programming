d93d3d40d546602af2fd2e647b7025f5ae1af14c
gastonperego <gastonperego@gmail.com>
2023-06-17 08:29:18 -0700

    Print the alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,43 @@
  */
 int main(void)
 {
-	char c = 'a';
+	char a = a;
+	char b = b;
+	char c = c;
+	char d = d;
+	char e = e;
+	char f = f;
+	char g = g;
+	char h = h;
+	char i = i;
+	char j = j;
+	char k = k;
+	char l = l;
+	char m = m;
+	char n = n;
+	char o = o;
+	char p = p;
+	char q = q;
+	char r = r;
+	char s = s;
+	char t = t;
+	char u = u;
+	char v = v;
+	char w = w;
+	char x = x;
+	char y = y;
+	char z = z;
+	char sl = "\n";
 
-	for (c < 'z'; c++;)
-	{
-		putchar(c);
-	}
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar(i);
+
 	return (0);
 }
