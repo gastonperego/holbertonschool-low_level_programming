524922ec8f2479084b2b2323c192047a948116d3
gastonperego <gastonperego@gmail.com>
2023-06-17 12:06:50 -0700

    Print the añphabet without some letters

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main- Print the alphabet except some letters
+ *
+ *  Return: always zero
+ */
+int main(void)
+{
+	char a = 'a';
+	char z = 'z';
+
+	for (; a <= z && a != q && a != e; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
