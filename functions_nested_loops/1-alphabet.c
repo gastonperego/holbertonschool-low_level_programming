05b76a8cbbc3cd63dea50f42f1feb2c200ff955a
gastonperego <gastonperego@gmail.com>
2023-06-18 10:18:54 -0700

    Print the alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *  main- Print the alphabet followed by a new line
  *
  *  Return: 0
  */
+int main(void)
+{
+	char c[26] = "abcdefghijklmnopqrstuvwxyz";
 
+	_putchar(c);
+	_putchar('\n');
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 int *test (void);
+void print_alphabet(void);
