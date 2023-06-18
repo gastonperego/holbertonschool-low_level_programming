d903320b67e5b78667f325f8268b25824c002e43
gastonperego <gastonperego@gmail.com>
2023-06-18 08:54:33 -0700

    print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,10 +5,15 @@
  *  Return: 0
  */
 int main(void)
-{
-	char c[8] = "_putchar";
-	
-	_putchar(char c);
+{	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
