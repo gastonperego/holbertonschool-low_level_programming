e976fb6277ca503bff63605da64560fc9b251bd2
gastonperego <gastonperego@gmail.com>
2023-06-18 15:59:42 -0700

    Print 1 and + if the input is possitive, print -1 and - if its negative and print 0 0 otherwise

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,19 +8,21 @@
  */
 int print_sign(int n)
 {
+	int a;
+	
 	if (n > '0')
 	{
 		_putchar(43);
-		return (1);
+		a = '1';
 	}
 	if (n > '0')
 	{
 		_putchar(45);
-		return (-1);
+		a = '-1'
 	} if (n == '0')
 	{
 		_putchar('0');
-		return (0);
+		a = '0';
 	}
-	
+	return (a);
 }
