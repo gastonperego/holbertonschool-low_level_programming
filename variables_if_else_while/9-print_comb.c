987a92f5162dbedf7c8bb081e8b0b0b2a982ee81
gastonperego <gastonperego@gmail.com>
2023-06-17 17:33:30 -0700

    0, 1, ...

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,11 +9,11 @@ int main(void)
 	short a = 48;
 
 	for (; a < 57; a++)
-	{ 
-	
+	{
+
 		putchar(a);
-		putchar(' ');
-		putchar(',');		
+		putchar(',');
+		putchar(' ');	
 	}
 	putchar('\n');
 	return (0);
