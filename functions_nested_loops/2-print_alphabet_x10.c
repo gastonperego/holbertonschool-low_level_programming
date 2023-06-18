e4b5658013fb47adaeea12a5314714fd0d924b4c
gastonperego <gastonperego@gmail.com>
2023-06-18 12:32:37 -0700

    Print the alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- Print the alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	char A = '0';
+
+	for (; A <= 57 && a = 'z'; A++)
+	{
+		for (; a <= 'z';)
+		{
+			if (a < 'z')
+			{
+				a++;
+			} else
+				a = 'a'
+		}
+		_putchar(a);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char c);
 int *test (void);
 void print_alphabet(void);
+void print_alphabet_x10(void);
