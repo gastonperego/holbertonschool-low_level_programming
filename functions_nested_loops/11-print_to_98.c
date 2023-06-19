4603ff923fd9229cefa5f7c530fa756f558258c6
gastonperego <gastonperego@gmail.com>
2023-06-19 09:22:20 -0700

    print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98- Printo to 98
+ *
+ * @n: int
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	} else if (n > 98)
+	{
+		for (; n > 98; n++)
+		{
+			printf("%d, ");
+		}
+	} else if (n == 98)
+	{
+		printf(98);
+	}
+}
