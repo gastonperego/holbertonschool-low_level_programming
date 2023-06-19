78e1b93c6bab1c118955a0b542c4db419929a0f4
gastonperego <gastonperego@gmail.com>
2023-06-19 05:14:33 -0700

    Sign of the input

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,19 +8,16 @@
  */
 int print_sign(int n)
 {
-	int a;
-
 	if (n > '0')
 	{
-		a = -1;
-		_putchar(43);
-	} else if (n > '0')
+		_putchar('+');
+		return(1);
+	} else if (n < '0')
 	{
-		a = -1;
-		_putchar(45);
-	} else if (n == '0')
-	{
-		_putchar('0');
+		_putchar('-');
+		return(-1);
 	}
-	return (a);
+	} else 
+		_putchar('0');
+		return('0');
 }
---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ *  _abs- kpfe
+ *
+ *  Return: 0
+ */
+
