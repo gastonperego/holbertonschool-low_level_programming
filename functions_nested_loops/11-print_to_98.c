4c728454c13b68c60c5754d11d2620deacf1bbe2
gastonperego <gastonperego@gmail.com>
2023-06-19 09:33:02 -0700

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		}
 	} else if (n > 98)
 	{
-		for (; n > 98; n++)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
@@ -25,4 +25,5 @@ void print_to_98(int n)
 	{
 		printf("98");
 	}
+	printf('\n');
 }
