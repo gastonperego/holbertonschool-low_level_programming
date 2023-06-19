bf3e2a3b3b24d561c9c2719272af742886f1a6b6
gastonperego <gastonperego@gmail.com>
2023-06-19 06:31:01 -0700

    Print every minute of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  jack_bauer- Prints every minute of the day
+ *
+ *  Return: 0
+ */
+void jack_bauer(void);
+{
+	int a = '0';
+	int b = '0';
+	
+
+	for (; a < 24; a++)
+	{
+		for (; b < 60; b++)
+		{
+			_putchar('0' + (a / 10));
+			_putchar('0' + (a % 10));
+			_putchar(':');
+			_putchar('0' + (a / 10));
+			_putchar('0' + (a % 10));
+			_putchar('\n');
+		}
+	}
+}
