87712f5c6ecca950de0542c019f0af828627e963
gastonperego <gastonperego@gmail.com>
2023-06-20 12:58:47 -0700

    print smth

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- print 0123456789abcde
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'a'; b < 'g'; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
