d8434f2d72babd0f16c2273de93c787d0a7bbf7d
gastonperego <gastonperego@gmail.com>
2023-06-19 17:28:27 -0700

    print numbers from 0 to 9 without 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * print_most_numbers- Print numbers from 0 to 9 without 2 and 4
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	for (; a < '9'; a++)
+	{
+		if (a != '2' && != '4')
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
+
