8899cbeb28439dd7263cfa0bd1ff8d69dbf573e3
gastonperego <gastonperego@gmail.com>
2023-06-19 19:32:53 -0700

    diagonal line

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,12 +13,16 @@ void print_diagonal(int n)
 	if (n > 0)
 	{
 		_putchar(92);
+		_putchar('\n');
 		for (; m < n; m++)
 		{
+			for (; m < n;)
+			{
 			_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+			}
 		}
-		_putchar(92);
-		_putchar('\n');
 	} 
 	_putchar('\n');
 }
