00e55e6cc22d8e0a84f9392ec689c8eb1346777f
gastonperego <gastonperego@gmail.com>
2023-06-20 09:41:58 -0700

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main- f
+ *
+ * Return: 0
+ */
+void main(void)
+{
+	int n;
+
+	for (n = 0; n <= 100; n++)
+	{
+		if ((n % 3) = 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if ((n % 5) = 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		} else
+		{
+			_putchar(n);
+		}
+	}
+}
+
