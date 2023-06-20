8b0fa172c659188768880364053c98a334c19e0f
gastonperego <gastonperego@gmail.com>
2023-06-19 17:15:17 -0700

    print numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers- Print from 0 to 9
+ *
+ * REturn: 0
+ */
+void print_numbers(void)
+{
+	int a = '0';
+
+	for (; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -14,3 +14,4 @@ void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
