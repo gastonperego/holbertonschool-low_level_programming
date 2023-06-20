bd77f059d6a179c6ce94ade0df113a1b6b33d07f
gastonperego <gastonperego@gmail.com>
2023-06-20 07:20:57 -0700

    square of #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square- print a square of #
+ *
+ * @size: size of the square
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int a;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0;a < size; size++)
+		{
+			_putchar('#');
+		}
+		for (; a == size;)
+		{
+			_putchar('\n');
+		}
+	}
+}		
