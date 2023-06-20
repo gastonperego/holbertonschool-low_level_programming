1b589bfb7d4fce8a94252ea5a7cc79aad6d05968
gastonperego <gastonperego@gmail.com>
2023-06-20 10:42:02 -0700

    FizzBuzz

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle- prinst a triangle
+ *
+ * @size: input
+ *
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int n, m;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (n = 0; n < size; n++)
+		{
+			for (m = 0; m < size; m++)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+		}
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -20,4 +20,4 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void fizzbuzz(void);
+void print_triangle(int size);
