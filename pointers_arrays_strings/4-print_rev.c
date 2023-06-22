087918dd9b647c43c6ae5857056c18c55a1eebec
gastonperego <gastonperego@gmail.com>
2023-06-22 07:03:25 -0700

    print a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev- print a string in reverse
+ *
+ * @s: the string
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	for (; s[c] != '\0';);
+	{
+		c++;
+	}
+	for (; s[c] != '\0', c--)
+	{
+		_putchar(c);
+	}
+	putchar('\0');
+}
