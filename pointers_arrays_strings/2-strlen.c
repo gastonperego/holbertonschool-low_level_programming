f0c6e27b68a29c4a97f2ce7c0406882ded1c7857
gastonperego <gastonperego@gmail.com>
2023-06-22 06:34:57 -0700

    Count the characters of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen- return the length of a string
+ *
+ * @s: s
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = '0'; s != '\0';)
+	{
+		c++;
+	}
+	return (c);
+}
