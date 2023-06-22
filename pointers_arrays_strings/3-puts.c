c1ad7acda3998226f23d047b54ecdcbe36e8f8c1
gastonperego <gastonperego@gmail.com>
2023-06-22 06:53:43 -0700

    print a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts- prints a string in the st output
+ *
+ * @str: string
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int c = 0;
+
+	for (; str[c] != '\0'; c++)
+	{
+		_putchar(str)
+	}
+}
