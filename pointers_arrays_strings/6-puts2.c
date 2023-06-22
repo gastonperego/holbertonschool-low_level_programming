eef66ab649110fa9272de29b666be737c75e0b58
gastonperego <gastonperego@gmail.com>
2023-06-22 10:43:17 -0700

    print the pair "lockers" of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+/**
+ * puts2- print the pair "lockers" of a string
+ *
+ * @str: string
+ *
+ * Return: 0
+ */
+void puts2(char *str);
+{
+	int c;
+
+	for (c = 0; str[c] != '\0';)
+	{
+		c++
+	}
+	while (0 < c)
+	{
+		_putchar(str[c]);
+		c+2;
+	}
+}
