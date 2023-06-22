a1e1cb94e8d3d967fe8c86330c097f29b8d07c48
gastonperego <gastonperego@gmail.com>
2023-06-22 05:25:41 -0700

    Swap the values of two variables using pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int- swap the value of two integers
+ *
+ * @a: pointer to integer
+ * @b: another pointer to an integer
+ *
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
