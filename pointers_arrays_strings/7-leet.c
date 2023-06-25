5a96c332aa23879131774680ca6c0f142c772d68
gastonperego <gastonperego@gmail.com>
2023-06-25 16:20:38 -0700

    1377

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - Encodes the string into 1337
+ *
+ * @s:string
+ *
+ * Return: char
+ */
+char *leet(char *s)
+{
+	int count = 0;
+	int count2;
+	char letters[5] = {'a', 'e', 'o', 't', 'l'};
+	char nums[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[count] != '\0')
+	{
+		for (count2 = 0; count2 < 5; count2++)
+		{
+			if (s[count] == letter[count2] || s[count] == letter[count2] - 32)
+			{
+				s[count] = num[count2];
+			}
+		}
+		count++;
+	}
+	return (s);
+}
