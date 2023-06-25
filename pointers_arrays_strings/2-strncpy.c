b12eeabe8a3bad34bc47ed8fc39f531ef6be001a
gastonperego <gastonperego@gmail.com>
2023-06-25 12:49:13 -0700

    copy some of the character of a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy- copie some characters of a string
+ *
+ * @dest: destiny string
+ * @src: source string
+ * @n: number of characters to be copied
+ *
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int bcount = 0;
+
+	for (; bcount < n && src[bcount] != '\0'; bcount++)
+	{
+		dest[bcount] = src[bcount];
+	}
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,3 +23,4 @@ void print_square(int size);
 void print_triangle(int size);
 char *_strcpy(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
