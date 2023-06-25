17d299c1acc02ae751147017f9b5a611653bccdf
gastonperego <gastonperego@gmail.com>
2023-06-25 14:53:26 -0700

    revert an array

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main,h"
+/**
+ * string_toupper- change all lowercase to upper
+ *
+ * @s: string
+ *
+ * Retrun: char
+ */
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		if (s[count] <= 122 && a[count] >= 97)
+		{
+			s[count] = s[count] -32;
+		}
+	}
+	return (s);
+}
