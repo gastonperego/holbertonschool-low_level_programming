496621e4958180d24265ca243edae14760318d44
gastonperego <gastonperego@gmail.com>
2023-06-25 14:33:26 -0700

    revert an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * reverse_array- revert an array
+ *
+ * @a:array
+ * @n: number of elemnts of the array
+ *
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+	int inicio = 0;
+	int final = 0;
+	int z;
+
+	while (final < n)
+	{
+		final++;
+	}
+	for (; inicio < final;)
+	{
+		z = a[inicio];
+		a[inicio] = a[final];
+		a[final] = z;
+		inicio++, final++;
+	}
+}
