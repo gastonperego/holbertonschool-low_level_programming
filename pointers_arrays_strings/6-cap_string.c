e57f78596b7430e6239324627347929e19d79c71
gastonperego <gastonperego@gmail.com>
2023-06-25 15:37:54 -0700

    Capitalize all the words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string- capitalizes all the words
+ *
+ * @s: string
+ *
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		if (s[count] <= 122 && s[count] >= 97)
+		{
+			if (s[count - 1] == ' ' || s[count - 1] == '.' || s[count - 1] == ';' ||
+			s[count - 1] == ',' || s[count - 1] == '!' || s[count - 1] == '"' ||
+			s[count - 1] == '?' || s[count - 1] == '(' || s[count - 1] == ')' ||
+			s[count - 1] == '{' || s[count - 1] == '}')
+			{
+				s[count] = s[count] - 32;
+			}
+		}
+	}
+	return (s);
+}
