8742a4f66e2bab274c7734a435ad91fb0efae32e
gastonperego <gastonperego@gmail.com>
2023-06-26 05:52:32 -0700

    fills the first n bytes of the memory area pointed to by s with the constant byte b

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset- fills n bytes of memory pointed by s with the constant byte b
+ *
+ * @s:area of memory where the vytes will be filled
+ * @n: number of bytes to be filled
+ * @b:constant byte
+ *
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		s[count] = b;
+	}
+	return (s);
+}
