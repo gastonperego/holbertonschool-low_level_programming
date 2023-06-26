b04d23817bdec1c4385820146342ef68ae44a4ff
gastonperego <gastonperego@gmail.com>
2023-06-26 16:12:25 -0700

    print a chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -11,11 +11,12 @@ void print_chessboard(char (*a)[8])
 	int row = 0;
 	int col;
 
-	for (; row <= 8; row++)
+	for (; row < 8; row++)
 	{
-		for (col = 0; col <= 8; col++)
+		for (col = 0; col < 8; col++)
 		{
 			_putchar(a[row][col]);
 		}
+		_putchar('\n');
 	}
 }
