9dfaa2dd3eb0fb14e6082395097c6205c53e63cc
gastonperego <gastonperego@gmail.com>
2023-06-26 07:05:28 -0700

    Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,8 +11,6 @@
 char *_strchr(char *s, char c)
 {
 	int count = 0;
-	int count2 = 0;
-	char m[];
 
 	while (s[count] != c)
 	{
@@ -20,12 +18,7 @@ char *_strchr(char *s, char c)
 	}
 	if (s[count] == c)
 	{
-		while (s[count] != '\0')
-		{
-		m[count2] = s[count];
-		count2++;
-		}
-		return (s);
+		return (&s[count]);
 	}
 	return ('\0');
 }
