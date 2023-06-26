9967b7645ce8a2b8eedb778a4d2ba2b1a9df20a8
gastonperego <gastonperego@gmail.com>
2023-06-26 14:30:01 -0700

    finds the first occurrence of the substring needle in the string haystack. The terminating null bytes (\0) are not compared

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -24,7 +24,7 @@ char *_strstr(char *haystack, char *needle)
 				{
 				address = &haystack[count];
 				}
-				if (needle[count2] == '\0')
+				if (needle[count2 + 1] == '\0')
 				{
 					return (address);
 				}
