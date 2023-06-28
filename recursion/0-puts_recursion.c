2360aed6c36094755913569462fbf73d77a3deae
gastonperego <gastonperego@gmail.com>
2023-06-28 15:40:52 -0700

    Print a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion-  prints a string, followed by a new line
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int count = 0;
+
+	if (s[count != '0'])
+	{
+		_putchar(s[count];
+		_putchar(s[count + 1]);
+	}
+}
