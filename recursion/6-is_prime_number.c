c6719226bbbc5692fa9dd0216f53a6f3c2a55a19
gastonperego <gastonperego@gmail.com>
2023-06-29 10:31:25 -0700

    return 1 si es numero primo, return 0 otherwise

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * is_prime_number- retorna 1 si el numero es primo y retorna 0 si no
+ *
+ * @n: el numero
+ *
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	if (n % n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
