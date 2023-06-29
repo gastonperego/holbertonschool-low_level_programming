9b7a524e37f273005a53d17e02b50c8dea2b26eb
gastonperego <gastonperego@gmail.com>
2023-06-29 05:44:37 -0700

    contar con recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion- returns the length of a string.
+ *
+ * @s: string
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_reecursion);
+	}
+}
