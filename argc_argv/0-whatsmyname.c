224be09753490e05b380e7a40a3cf17620037e69
gastonperego <gastonperego@gmail.com>
2023-06-30 09:37:16 -0700

    argc argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main- prints the name of the function
+ * whats_my_name- prints the name of the function
  *
  * @argc: numero de caractres de la string
  * @argv: string
  *
  * Return: 0
  */
-int main(int argc, char *argv)
+int whats_my_name(int argc, char *argv)
 {
 	printf("argv[0]");
 	return (0);
