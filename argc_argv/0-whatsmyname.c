71e3bbe2bbfe25a10af509ff5c91c72b65502998
gastonperego <gastonperego@gmail.com>
2023-07-01 17:02:52 -0700

    print the name of the command

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,12 +2,11 @@
 /**
  * whats_my_name- prints the name of the function
  *
- * @argc: numero de caractres de la string
  * @argv: string
  *
  * Return: 0
  */
-int whats_my_name(int argc, char *argv[])
+int whats_my_name(char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
