38d138310dae139f1575bcbbe07fc077462db7b9
gastonperego <gastonperego@gmail.com>
2023-07-01 19:10:48 -0700

    Print the number of aruments given

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,10 +4,11 @@
  * main- print then umber of arguments given
  *
  * @argc: count the arguments
+ * @argv: vector
  *
  * Return: 0
  */
-int main(int argc)
+int main(int argc, char *argv)
 {
 	printf("%d", argc);
 	return (0);
