ca989a1376e66abf9e95cea20deaf25df2adfe0e
gastonperego <gastonperego@gmail.com>
2023-07-01 19:06:49 -0700

    Print the number of aruments given

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+#include <stdio.h>
+/**
+ * main- print then umber of arguments given
+ *
+ * @argc: count the arguments
+ * @argv: vector of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	printf("%d", argc);
+	Return (0);
+}
