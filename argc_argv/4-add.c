dc83c5b0ede8e5f5a8af3a001bffc88d1b753ece
gastonperego <gastonperego@gmail.com>
2023-07-02 09:34:06 -0700

    sum all the arguments given

---
+++ argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main- adds all the arguments given
+ *
+ * @argc: count the arguments
+ * @argv: the string
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int count2;
+	char *aux;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (count = 1; count < argc; count++)
+	{
+		aux = argv[count];
+		for (count2 = 0; aux[count2] != '\0', count2++)
+		{
+			if (aux[count] < 48 || aux[count] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += argv[count];
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
