f54af83a2b264280dd2e3e987eb80a7dad76f00e
gastonperego <gastonperego@gmail.com>
2023-07-01 19:02:33 -0700

    Print the name of the command used

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * whats_my_name- prints the name of the function
+ * main- prints the name of the function
  *
  * @argv: string
  * @argc: number of elements of the string
  *
  * Return: 0
  */
-int whats_my_name(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
 	return (0);
