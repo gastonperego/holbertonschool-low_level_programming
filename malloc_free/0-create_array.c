93d191fe219b90868da9955c74fad625f4d193d6
gastonperego <gastonperego@gmail.com>
2023-07-05 07:15:25 -0700

    create an array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int count;
 
-	s = malloc(sizeof(int) * size);
+	s = malloc(sizeof(char) * size);
 	if  (s == NULL || size == 0)
 	{
 		return ('\0');
