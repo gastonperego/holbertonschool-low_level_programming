14e27086c8d59145644c94cac5ea8d330d968c3c
gastonperego <gastonperego@gmail.com>
2023-07-05 08:31:02 -0700

    return a pointer to a copy of a given string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup- return a pointer to a copy of a string
+ *
+ * @str: string to copy
+ *
+ * Return: puntero
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int count = 0;
+	int count2 = 0;
+
+	if (str == NULL)
+	{
+		return ('\0');
+	}
+	while (*str)
+	{
+		count++;
+	}
+
+	s = malloc(sizeof(char) * count);
+	if (s == NULL)
+	{
+		return ('\0');
+	}
+	for (; count2 <= count; count2++)
+	{
+		s[count2] = str[count2];
+	}
+	return (s);
+}
