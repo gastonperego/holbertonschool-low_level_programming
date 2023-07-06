d3c19ffb09ebd6df001e0040191954501f7acf3a
gastonperego <gastonperego@gmail.com>
2023-07-06 05:45:18 -0700

    concatenate 2 strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,6 +15,7 @@ char *str_concat(char *s1, char *s2)
 	int count = 0;
 	int count2 = 0;
 	int count3 = 0;
+	int count4 = 0;
 
 	for (; s1[count] != '\0';)
 	{
@@ -39,12 +40,12 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (; count3 < count; count3++)
 	{
-		s[count3] = s1[count];
+		s[count3] = s1[count3];
 	}
 	count2 = count2 + count3;
-	for (; count3 < count2; count3++)
+	for (; count3 < count2; count3++, count4++)
 	{
-		s[count3] = s2[count2];
+		s[count3] = s2[count4];
 	}
 	return (s);
 }
