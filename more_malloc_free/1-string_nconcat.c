fd2e01c58611a2d67ae454c3950b73164641f968
gastonperego <gastonperego@gmail.com>
2023-07-08 14:13:19 -0700

    concatenate a string to a limited number of character of the second one

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,25 +18,37 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len = 0;
 
 	if (s1)
-	for (; s1[c] != '\0';)
-		c++;
+	{
+		for (; s1[c] != '\0';)
+			c++;
+	}
 	if (s2)
+	{
 	for (; s2[c2] != '\0';)
 		c2++;
+	}
 	if (n < c2)
+	{
 		len = c + n;
+	}
 	else
+	{
 		len = c + c2;
+	}
 	s = malloc(sizeof(char) * len + 1);
 	if (s == NULL)
+	{
 		return (NULL);
-
+	}
 	if (s1)
-	for (c = 0; s1[c] != '\0'; c++)
-		s[c] = s1[c];
+	{
+		for (c = 0; s1[c] != '\0'; c++)
+			s[c] = s1[c];
+	}
 	if (s2)
+	{
 	for (c2 = 0; c < n; c++, c2++)
 		s[c] = s2[c2];
-
+	}
 	return (s);
 }
