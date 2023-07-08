1f8600dd2e8ab705b8396b79fad92b3a643ae9bf
gastonperego <gastonperego@gmail.com>
2023-07-08 12:13:05 -0700

    concatenate 2 strings with a variable number of character in the second string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,24 +15,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int c = 0;
 	unsigned int c2 = 0;
 	char *s;
+	unsigned int len = 0;
 
-	if (s1)
-		for (; s1[c] != '\0';)
-			c++;
-	if (s2)
-		for (; s2[c2] != '\0';)
-			c2++;
-	s = malloc(sizeof(char) * (c + n + 1));
-	if (s == NULL)
-		return ('\0');
-	if (s1)
-		for (c = 0; s1[c] != '\0'; c++)
-			s[c] = s1[c];
-	if (s2 && n < c2)
-		for (c2 = 0; c2 < n; c2++, c++)
-			s[c] = s2[c2];
-	else if (s2 != NULL && n >= c2)
-		for (c2 = 0; s2[c2] != '\0'; c2++, c++)
-			s[c] = s2[c2];
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (; s1[c] != '\0';)
+		c++;
+	for (; s2[c2] != '\0';)
+		c2++;
+	if (n < c2)
+		len = c + n;
+	else
+		len = c + c2;
+	s = malloc(sizeof(char) * (len + 1));
+	for (c = 0; s1[c] != '\0'; c++)
+		s[c] = s1[c];
+	for (c2 = 0; c < n; c++, c2++)
+		s[c] = s2[c2];
+
 	return (s);
 }
