2dd0d8bc62336277f69a306615a9b9e294d789b3
gastonperego <gastonperego@gmail.com>
2023-07-08 13:51:27 -0700

    create calloc function

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,11 +15,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *s;
 
 	if (nmemb < 1 || size < 1)
+	{
 		return ('\0');
+	}
 	s = malloc(a);
 	if (s == NULL)
+	{
 		return ('\0');
+	}
 	for (; c <= a; c++)
+	{
 		s[c] = 0;
+	}
 	return (s);
 }
