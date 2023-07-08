e9f76808d19872e57566f4a793d56aac6e45552a
gastonperego <gastonperego@gmail.com>
2023-07-08 07:21:59 -0700

    concatenate 2 strings with a variable number of character in the second string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,3 +11,31 @@
  * Return: pointer to the concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int c1 = 0;
+	int c2 = 0;
+	int *s;
+
+	if (s1)
+		for (; s[c1] != '\0';)
+			c1++;
+	if (s2)
+		for (; s2[c2] != '\0';)
+			c2++;
+	if (n < c2)
+		s = malloc(sizeof(char) * (c1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (c1 + c2 + 1));
+	if (s == NULL)
+		return ('\0');
+	if (s1)
+		for (c1 = 0; s1[c] != '\0'; c++)
+			s[c] = s1[c];
+	if (s2 && n < c2)
+		for (c2 = 0; c2 < n; c2++, c++)
+			s[c] = s2[c2];
+	if (s2 && n >= c2)
+		for (c2 = 0; s2[c2] != '\0'; c2++, c++)
+			s[c] = s2[c2];
+	return (s);
+}
