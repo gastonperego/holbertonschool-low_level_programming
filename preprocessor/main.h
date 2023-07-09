527f81669ab89b37127333811046549cebd6d308
gastonperego <gastonperego@gmail.com>
2023-07-09 08:05:04 -0700

    My first macro

---
+++ preprocessor/main.h
@@ -0,0 +1,8 @@
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
