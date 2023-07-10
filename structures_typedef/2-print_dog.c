112f0590627e8a6992f1f8ceb31c1051a3d5b2c5
gastonperego <gastonperego@gmail.com>
2023-07-10 13:56:26 -0700

    float has no null

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,10 +15,8 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == NULL)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
