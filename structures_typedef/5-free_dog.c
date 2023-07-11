85153b00fada1375e6cfa54aaaafd900547e9a58
gastonperego <gastonperego@gmail.com>
2023-07-11 14:14:11 -0700

    free thedog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib,h>
+/**
+ * free_dog- frees the dog
+ *
+ * @d: dog
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
