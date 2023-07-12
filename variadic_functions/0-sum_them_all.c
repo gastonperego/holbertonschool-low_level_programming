027a13e1ab929bc8d7527bd1d162f259de6f0296
gastonperego <gastonperego@gmail.com>
2023-07-12 14:53:33 -0700

    My first variadic function

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+/**
+ * sum_them_all- sums all its parameters except the first one
+ *
+ * @n: first parameter(it is the number of parameters to sum)
+ *
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int s = 0;
+	int count;
+
+	va_start(args, n);
+
+	for (count = 0; count < n; count++)
+	{
+		s += va_arg(args, int);
+	}
+	va_end(args);
+	return (s);
+}
