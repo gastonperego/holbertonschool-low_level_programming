aeef641838db4c2191aeaf2bf129ba12f754df85
gastonperego <gastonperego@gmail.com>
2023-07-13 06:59:16 -0700

    Fix the special case of a null string as the last one

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -19,10 +19,22 @@ op_mul(int a, int b)
 int
 op_div(int a, int b)
 {
-	return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+		return (a / b);
 }
 int
 op_mod(int a, int b)
 {
-	return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+		return (a % b);
 }
