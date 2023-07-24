637dbf6fcfcf2ec9da2aabf53a1c3d169af3e35a
gastonperego <gastonperego@gmail.com>
2023-07-24 05:38:40 -0700

    docs: description added for struct list_s

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,14 @@
 #ifndef HEADERFILE
 #define HEADERFILE
 #include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
 typedef struct list_s
 {
