d295fb99abc5554b0dc88e1c2dda8ecb49d338af
gastonperego <gastonperego@gmail.com>
2023-07-26 04:36:47 -0700

    fix: error with the prev pointer

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->next = tmp;
-	new->prev = *(head);
+	new->prev = NULL;
+
+	if (tmp != NULL)
+	{
+		tmp->prev = new;
+	}
 
 	(*head) = new;
 
