2e5d0c05bee450e42b1c16f507a49e3cf88b39d0
gastonperego <gastonperego@gmail.com>
2023-07-26 06:57:39 -0700

    feat: insert_dnodeint

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_dnodeint_at_index- inserts a new node at the given position of a
+ * doubly linked list
+ *
+ * @h: pointer to the firat node of the list
+ * @idx: position where the node has to be added
+ * @n: data of the new node
+ *
+ * Return: pointer to the first node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h;
+	dlistint_t *new;
+	unsigned int count = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (!*h)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		*h = new;
+	}
+	else
+	{
+		while (tmp && count != idx)
+		{
+			tmp = tmp->next;
+			count++;
+		}
+		if (tmp->next == NULL)
+		{
+			new->next = NULL;
+			new->prev = tmp;
+			tmp->next = new;
+		}
+		else
+		{
+			new->next = tmp->next;
+			new->prev = tmp;
+			tmp->next = new;
+		}
+	}
+	return (*h);
+}
