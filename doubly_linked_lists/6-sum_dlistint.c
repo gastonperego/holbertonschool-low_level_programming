66fa3a875b2c4d9532f54505d00f50478279914d
gastonperego <gastonperego@gmail.com>
2023-07-26 05:56:29 -0700

    feat: sum_dlistint

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * sum_dlistint- sums all the data(ints) of a doubly linked list
+ *
+ * @head: pointer to the first node of the list
+ *
+ * Return: result of the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int res = 0;
+	dlistint_t *tmp = head;
+
+	while (tmp->next != NULL)
+	{
+		res += tmp->n;
+		tmp = tmp->next;
+	}
+	res += tmp->n;
+	return (res);
+}
