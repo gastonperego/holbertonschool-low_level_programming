bac5af362f2ac105851649bcc097ce1bd3beebe6
gastonperego <gastonperego@gmail.com>
2023-07-27 12:53:16 -0700

    fix: bugs

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,42 +17,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count = 0;
 	dlistint_t *aux;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (tmp && count != (idx - 1))
+	{
+		tmp = tmp->next;
+		count++;
+	}
+
+	if (count < idx)
+		return (NULL);
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
+	new->next = tmp->next;
+	new->prev = tmp;
+	tmp->next = new;
+	tmp->next->prev = new;
 
-	if (!*h)
-	{
-		new->next = NULL;
-		new->prev = NULL;
-		*h = new;
-	}
-	else
-	{
-		while (tmp && count != (idx - 1))
-		{
-			tmp = tmp->next;
-			count++;
-		}
-		aux = tmp->next;
-		tmp = tmp->next;
-		if (count < idx)
-			return (NULL);
-		if (tmp->next == NULL)
-		{
-			new->next = NULL;
-			new->prev = tmp;
-			tmp->next = new;
-		}
-		else
-		{
-			new->next = tmp;
-			new->prev = tmp->prev;
-			tmp->prev = new;
-			aux->next = new;
-		}
-	}
 	return (*h);
 }
