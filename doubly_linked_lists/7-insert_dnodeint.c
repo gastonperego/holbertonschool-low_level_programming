7245f9057af593aa06a19bc7126ff7076c373c61
gastonperego <gastonperego@gmail.com>
2023-07-28 05:28:38 -0700

    fix: bugs

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,6 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h;
 	dlistint_t *new;
+	dlistint_t *aux;
 	unsigned int count = 0;
 
 	if (idx == 0)
@@ -35,11 +36,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
+	aux = tmp;
 	new->n = n;
 	new->next = tmp->next;
 	new->prev = tmp;
 	tmp->next = new;
-	tmp->next->prev = new;
+	aux->next->prev = new;
 
 	return (*h);
 }
