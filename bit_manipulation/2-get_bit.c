3237897df2d627237c3f4955b677f791d8305df3
gastonperego <gastonperego@gmail.com>
2023-07-30 13:19:40 -0700

    fix: outranged index case

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -10,23 +10,16 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int a = 1;
-	int res = 0;
-	unsigned int len = 0;
-	unsigned long int copy = n;
+	int bit = 0;
+	int max = sizeof(unsigned long int * 8);
 
-	while (copy != 0)
-	{
-		copy = copy >> 1;
-		len++;
-	}
-	if (index > len)
+	if (index < max)
+		a = a << index;
+	else
 		return (-1);
 
-	if (index > 0)
-		a = a << index;
-
 	if ((n & a) != 0)
-		res = 1;
+		bit = 1;
 
-	return (res);
+	return (bit);
 }
