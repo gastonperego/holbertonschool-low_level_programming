b0abba27495c082c28958746d6b404b37b9ec9f1
gastonperego <gastonperego@gmail.com>
2023-08-01 10:05:18 -0700

    fix: permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	len = strlen(text_content);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd < 0)
 		return (-1);
