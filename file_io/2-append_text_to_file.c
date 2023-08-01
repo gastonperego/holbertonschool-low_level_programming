5c9d4b9e97277663e72fcfc02f35408f3a9195e2
gastonperego <gastonperego@gmail.com>
2023-08-01 10:27:51 -0700

    fix: ortografic problem

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		text_content = "";
 	len = strlen(text_content);
 
-	fd = open(filename, O_RDWR | O_APENND);
+	fd = open(filename, O_RDWR | O_APPEND);
 
 	if (fd < 0)
 		return (-1);
