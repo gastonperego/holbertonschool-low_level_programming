07c0f2e6ffaa231ae6cc3f261890209f1c5fda5c
gastonperego <gastonperego@gmail.com>
2023-08-01 10:03:09 -0700

    feat: creat_file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+/**
+ * create_file- creates a file
+ *
+ * @filename: filename
+ * @text_content: content
+ *
+ * Return: 1 if succeed, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len;
+	ssize_t wrote;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	len = strlen(text_content);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+
+	if (fd < 0)
+		return (-1);
+
+	wrote = write(fd, text_content, len);
+	if (wrote == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
