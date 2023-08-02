a892bd53bc75e555fe331e15ea69d37a1399c081
gastonperego <gastonperego@gmail.com>
2023-08-02 04:29:31 -0700

    fix: some bugs

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
 	int fd, fd1, close1, close2;
 	char buf[100000];
-	ssize_t readed;
+	ssize_t readed, wrote;
 
 	if (argc != 3)
 	{
@@ -29,22 +29,30 @@ int main(int argc, char *argv[])
 
 	if (fd < 0 || readed < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	fd1 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	write(fd1, buf, readed);
-	if (fd1 < 0)
+	wrote write(fd1, buf, readed);
+	if (fd1 < 0 || wrote < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 		exit(99);
 	}
 
 	close1 = close(fd);
 	close2 = close(fd1);
-	if (close1 < 0 || close2 < 0)
-		return (-1);
+	if (close1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d". fd);
+		exit(100);
+	}
+	if (close2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
+		exit(100);
+	}
 
 	return (0);
 }
