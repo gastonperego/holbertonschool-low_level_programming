ed5276546e73703994b90794ec9c0c3741f7d81f
gastonperego <gastonperego@gmail.com>
2023-08-03 07:25:09 -0700

    feat: create_hash_table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create- creates a hash table
+ *
+ * @size: size of he table
+ *
+ * Return: pointer to the table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ptr;
+	unsigned long int count = 0;
+
+	ptr = malloc(sizeof(hash_table_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->array = malloc((sizeof(hash_node_t) * size));
+	while (count < size)
+	{
+		ptr->array[count] = NULL;
+		count++;
+	}
+	ptr->size = size;
+	return (ptr);
+}
