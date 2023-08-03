2cb7c682a9d802cf0aa94a4e9341b32c58ef9a97
gastonperego <gastonperego@gmail.com>
2023-08-03 09:07:46 -0700

    fix: wrong memory assignment

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr->array = malloc((sizeof(hash_node_t) * size));
+	ptr->array = malloc((sizeof(hash_node_t *) * size));
 	ptr->size = size;
 
 	return (ptr);
