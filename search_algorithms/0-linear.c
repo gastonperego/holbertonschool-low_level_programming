358b3afdd90b4b3facb185212b3d9e0f03d00514
gastonperego <gastonperego@gmail.com>
2023-09-13 12:44:57 +0000

    feat: linear, search_algos and README

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+/**
+* 
+*/
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+
+    if (!array)
+        return(-1);
+    
+    for (; i < size - 1 && array[i] != value; i++)
+    {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+    }
+    if (!array[i] || array[i] != value)
+        return(-1);
+    
+    printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+    return (i);
+}
