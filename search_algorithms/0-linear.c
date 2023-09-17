c1d7f7ac31890df1ee048526c23135ed9faa8bb5
gastonperego <gastonperego@gmail.com>
2023-09-17 17:52:47 +0000

    fix: betty style

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,14 +13,14 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (!array)
-		return(-1);
+		return (-1);
 
 	for (; i < size && array[i] != value; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 	if (!array[i] || array[i] != value)
-		return(-1);
+		return (-1);
 
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	return (i);
